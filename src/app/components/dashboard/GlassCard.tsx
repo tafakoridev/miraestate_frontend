f6b97274448efe0cbd17d4c790f6a0456b089741
tafakoriwork <tafakoriwork@gmail.com>
2024-02-02 16:30:42 +0330

    change in categories, add purposed fields

--- src/app/components/dashboard/GlassCard.tsx
+++ src/app/components/dashboard/GlassCard.tsx
@@ -9,11 +9,11 @@ interface GlassCardProps {
 }
 
 interface Category {
-    id: number;
-    title: string;
-    price: string;
-    parent_id: string;
-  }
+  id: number;
+  title: string;
+  price: string;
+  parent_id: string;
+}
 
 interface Field {
   title: string;
@@ -21,8 +21,23 @@ interface Field {
 }
 
 const GlassCard: React.FC<GlassCardProps> = ({ onClose, onSend, id }) => {
-  const [fields, setFields] = useState<Field[]>([{title: "", value: ""}]);
+  const [fields, setFields] = useState<Field[]>([{ title: "", value: "" }]);
   const [category, setCategory] = useState<Category>();
+  const [options, setOptions] = useState([
+    { title: "محل", value: "" },
+    { title: "قیمت", value: "" },
+    { title: "متراژ", value: "" },
+    { title: "تعداد اتاق", value: "" },
+    { title: "سن بنا", value: "" },
+    { title: "طبقه", value: "" },
+    { title: "جنس کف", value: "" },
+    { title: "سرمایش", value: "" },
+    { title: "سرویس بهداشتی", value: "" },
+    { title: "تامین آب گرم", value: "" },
+    { title: "جهت ساختمان", value: "" },
+    { title: "تعداد واحد هر طبقه", value: "" },
+    { title: "گرمایش", value: "" },
+  ]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,27 +49,35 @@ const GlassCard: React.FC<GlassCardProps> = ({ onClose, onSend, id }) => {
         if (response.ok) {
           const data = await response.json();
           setCategory(data.category);
-          if(data.category.fields) {
-            setFields(JSON.parse(data.category.fields))
+          if (data.category.fields) {
+            setFields(JSON.parse(data.category.fields));
           }
         } else {
-          console.error('Failed to fetch category:', response.statusText);
+          console.error("Failed to fetch category:", response.statusText);
         }
       } catch (error) {
-        console.error('Error fetching category:', error);
+        console.error("Error fetching category:", error);
       }
     };
 
     fetchData();
   }, [id]);
+
   const handleAddField = () => {
-    setFields((prevFields) => [...prevFields, {title: "", value: ""}]);
+    setFields((prevFields) => [...prevFields, { title: "", value: "" }]);
+  };
+
+  const addToList = (el: any, i: any) => {
+    let opts = options;
+    opts.splice(i, 1);
+    setOptions([...opts])
+    setFields((prevFields) => [...prevFields, { title: el.title, value: "" }]);
   };
 
   const handleInputChange = (index: number, value: string) => {
     setFields((prevFields) => {
       const newFields = [...prevFields];
-      newFields[index] = {title: value, value: ""};
+      newFields[index] = { title: value, value: "" };
       return newFields;
     });
   };
@@ -68,7 +91,17 @@ const GlassCard: React.FC<GlassCardProps> = ({ onClose, onSend, id }) => {
       </div>
       <div className="text-black">
         {/* Your card content goes here */}
-        <h2 className="text-2xl font-bold mb-4">مدیریت فیلدهای {category?.title}</h2>
+        <h2 className="text-2xl font-bold mb-4">
+          مدیریت فیلدهای {category?.title}
+        </h2>
+        <h4>فیلدهای پیشنهادی</h4>
+        <div className="flex gap-1 flex-wrap justify-center my-3">
+          {options.map((el: any, i: any) => (
+            <div onClick={() => addToList(el, i)} className="cursor-pointer py-1 px-2 text-xs bg-white border-blue-600 border rounded-md whitespace-nowrap hover:bg-blue-100">
+              {el.title}
+            </div>
+          ))}
+        </div>
         <div className="flex flex-wrap ">
           {fields.map((field, index) => (
             <div key={index} className="mb-2">
